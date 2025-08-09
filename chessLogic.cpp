4e9ed70790019163a7819af29f02857c5608a707
B3R63R <104020923+B3R63R@users.noreply.github.com>
2025-08-09 20:36:18 +0200

    Fill header file and Redesign chessLogic.cpp
    -Written declaration of used classes in .cpp
    -Changed declaration of classed to definiotions
    -Written PotentialMoves() method for pawn class

--- chessLogic.cpp
+++ chessLogic.cpp
@@ -1,140 +1,156 @@
+#include "chessLogic.h"
 #include <array>
 #include <string>
 #include <memory>
 #include <iostream>
 #include <map>
-class Piece {
-public:
-    std::string color;
-    int row;
-    int column;
-    std::string name;
-    bool hasMoved;
+#include <tuple>
 
-    Piece(const std::string& color, int row, int column, const std::string& name):
-        color(color), row(row), column(column), name(name), hasMoved(false) {}
+Piece::Piece(const std::string& color, int row, int column, const std::string& name)
+    : color(color), row(row), column(column), name(name), hasMoved(false) {}
 
-    std::string getSymbol() {
+    std::string Piece::getSymbol() {
         return color+name;
     }
-    std::string getColor() {
+    std::string Piece::getColor() {
         return color;
     }
-    std::string getName() {
+    std::string Piece::getName() {
         return name;
     }
-    int getColumn() {
+    int Piece::getColumn() {
         return column;
     }
-    int getRow() {
+    int Piece::getRow() {
         return row;
     }
-    void setPosition(int Row, int Column) {
+    void Piece::setPosition(int Row, int Column) {
         row = Row;
         column = Column;
         hasMoved = true;
     }
 
-};
+Pawn::Pawn(const std::string& color, int row, int column):
+    Piece(color, row, column, "P") {}
 
-class Pawn : public Piece {
-public:
-    Pawn(const std::string& color, int row, int column):
-        Piece(color, row, column, "P") {}
-
-};
-
-class Knight : public Piece {
-public:
-    Knight(const std::string& color, int row, int column):
-        Piece(color, row, column, "N") {}
-};
-
-
-class Rook : public Piece {
-public:
-    Rook(const std::string& color, int row, int column):
-        Piece(color, row, column, "R") {}
-};
-
-class Bishop : public Piece {
-public:
-    Bishop(const std::string& color, int row, int column):
-        Piece(color, row, column, "B") {}
-};
-
-class Queen : public Piece {
-public:
-    Queen(const std::string& color, int row, int column):
-        Piece(color, row, column, "Q") {}
-};
-
-class King : public Piece {
-public:
-    King(const std::string& color, int row, int column):
-        Piece(color, row, column, "K") {}
-};
-
-class Board {
-public:
-    std::array<std::array<std::unique_ptr<Piece>, 8>, 8> board;
-    Board() {
-        for (int row=0; row<8; row++) {
-            for (int col = 0; col <8; col++) {
-                board[row][col] = nullptr;
+    std::vector<std::tuple<int, int>> Pawn::getPotentialMoves(const Board& board) {
+        std::vector<std::tuple<int, int>> potentialMoves;
+        const auto& boardArray = board.getBoard();
+        std::vector<std::tuple<int, int>> pawnDirections = {{1, 0 }, {2, 0}, {1, 1}, {1, -1}};
+        for (const auto& [row, col] : pawnDirections) {
+            int newRow = 0;
+            int newCol = 0;
+            //Reject moves through 2 fields if pawn has moved
+            if (row == 2 && this->hasMoved == true) {
+                continue;
+            }
+            //Set directions based on current color
+            if (this->color == "w") {
+                newRow = this->getRow() + row;
+                newCol = this->getColumn() + col;
+            }
+            else {
+                newRow = this->getRow() + row*(-1);
+                newCol = this->getColumn();
+            }
+            //Check if moves is in chessboard scope
+            if (0 <= newRow && newRow < 8 && 0 <= newCol && newCol < 8) {
+                //Check moves on empty squere (the same column)
+                if (boardArray[newRow][newCol] == nullptr && newCol == this->getColumn()) {
+                    // move by one square
+                    if (row != 2) {
+                        potentialMoves.push_back({newRow, newCol});
+                    }
+                    //Check moves through 2 squares (indirect transition field must be empty)
+                    else {
+                        if (this->getColor() == "w") {
+                            if (boardArray[newRow-1][newCol] == nullptr) {
+                                potentialMoves.push_back({newRow, newCol});
+                            }
+                        }
+                        else {
+                            if (boardArray[newRow+1][newCol] == nullptr) {
+                                potentialMoves.push_back({newRow, newCol});
+                            }
+                        }
+                    }
+                }
+            }
+            //Check beating piece diagonally (squere occupied by diffent color piece)
+            else if (boardArray[newRow][newCol] != nullptr && boardArray[newRow][newCol]->getColor() != this->getColor()) {
+                potentialMoves.push_back({newRow, newCol});
             }
         }
+        return potentialMoves;
+
     }
-    void display() {
-        /*
-        for (int row=0; row<8; row++) {
-            for (int col = 0; col <8; col++) {
-                if (board[row][col]) {
-                    std::cout << board[row][col] -> getSymbol() + " ";
-                }
-                else {
+
+
+Knight::Knight(const std::string& color, int row, int column):
+    Piece(color, row, column, "N") {}
+
+
+
+
+Rook::Rook(const std::string& color, int row, int column):
+    Piece(color, row, column, "R") {}
+
+
+Bishop::Bishop(const std::string& color, int row, int column):
+    Piece(color, row, column, "B") {}
+
+Queen::Queen(const std::string& color, int row, int column):
+    Piece(color, row, column, "Q") {}
+
+
+King::King(const std::string& color, int row, int column):
+    Piece(color, row, column, "K") {}
+
+
+Board::Board() {
+    for (int row=0; row<8; row++) {
+        for (int col = 0; col <8; col++) {
+            board[row][col] = nullptr;
+        }
+    }
+}
+const std::array<std::array<std::unique_ptr<Piece>, 8>, 8>& Board::getBoard() const { //przenoszenie tylko do odczytu
+    return board;
+    }
+void Board::display() {
+    for (int row = 7; row >= 0; row--) {
+        for (int col = 0; col <8; col++) {
+            if (board[row][col]) {
+                std::cout << board[row][col] -> getSymbol() + " ";
+            }
+            else {
                 std::cout << "-";
-                }
             }
-            std::cout << '\n';
         }
-        */
-
-        for (int row = 7; row >= 0; row--) {
-            for (int col = 0; col <8; col++) {
-                if (board[row][col]) {
-                    std::cout << board[row][col] -> getSymbol() + " ";
-                }
-                else {
-                    std::cout << "-";
-                }
-            }
-            std::cout << '\n';
-        }
+        std::cout << '\n';
     }
-    void setupPieces() {
-        //Pawns
-        for (int field = 0; field<8; field++) {
-            board[1][field] = std::make_unique<Pawn>("w", 1, field);
-            board[6][field] = std::make_unique<Pawn>("b", 6, field);
-
-        }
-        //Other pieces
-
-        std::map <std::string, int> piecesParams = {{"w", 0}, {"b", 7}};
-
-        for (auto itr = piecesParams.begin(); itr !=piecesParams.end(); itr++) {
-            board[itr->second][0] = std::make_unique<Rook>(itr->first, itr->second, 0);
-            board[itr->second][1] = std::make_unique<Knight>(itr->first, itr->second, 1);
-            board[itr->second][2] = std::make_unique<Bishop>(itr->first, itr->second, 2);
-            board[itr->second][3] = std::make_unique<Queen>(itr->first, itr->second, 3);
-            board[itr->second][4] = std::make_unique<King>(itr->first, itr->second, 4);
-            board[itr->second][5] = std::make_unique<Bishop>(itr->first, itr->second, 5);
-            board[itr->second][6] = std::make_unique<Knight>(itr->first, itr->second, 6);
-            board[itr->second][7] = std::make_unique<Rook>(itr->first, itr->second, 7);
-        }
+}
+void Board::setupPieces() {
+    // Pawns
+    for (int field = 0; field < 8; field++) {
+        board[1][field] = std::make_unique<Pawn>("w", 1, field);
+        board[6][field] = std::make_unique<Pawn>("b", 6, field);
     }
-};
+
+    // Other pieces
+    std::map <std::string, int> piecesParams = {{"w", 0}, {"b", 7}};
+
+    for (auto const& [color, row] : piecesParams) {
+        board[row][0] = std::make_unique<Rook>(color, row, 0);
+        board[row][1] = std::make_unique<Knight>(color, row, 1);
+        board[row][2] = std::make_unique<Bishop>(color, row, 2);
+        board[row][3] = std::make_unique<Queen>(color, row, 3);
+        board[row][4] = std::make_unique<King>(color, row, 4);
+        board[row][5] = std::make_unique<Bishop>(color, row, 5);
+        board[row][6] = std::make_unique<Knight>(color, row, 6);
+        board[row][7] = std::make_unique<Rook>(color, row, 7);
+    }
+}
 int main() {
     Board b;
     b.setupPieces();
--- chessLogic.h
+++ chessLogic.h
@@ -4,23 +4,69 @@
 #endif // CHESSLOGIC_H
 #include <string>
 #include <vector>
+#include <array>
+#include <string>
+#include <memory>
+#include <vector>
+#include <tuple>
+
+class Piece;
+
+class Board {
+public:
+    std::array<std::array<std::unique_ptr<Piece>, 8>, 8> board;
+
+    Board();
+    const std::array<std::array<std::unique_ptr<Piece>, 8>, 8>& getBoard() const;
+    void display();
+    void setupPieces();
+};
+
 class Piece {
 public:
-    Piece(std::string color, int row, int column, std::string name);
+    std::string color;
+    int row;
+    int column;
+    std::string name;
+    bool hasMoved;
+
+    Piece(const std::string& color, int row, int column, const std::string& name);
 
     std::string getSymbol();
-
     std::string getColor();
-
-    int getColum();
-
-    int getRow();
-
     std::string getName();
+    int getColumn();
+    int getRow();
+    void setPosition(int Row, int Column);
+};
 
-    void setPosition();
+class Pawn : public Piece {
+public:
+    Pawn(const std::string& color, int row, int column);
+    std::vector<std::tuple<int, int>> getPotentialMoves(const Board& board);
+};
 
-    //virtual std::vector<std::pair<int,int>> getPotentialMoves(Board& board) = 0;
-    //std::
+class Knight : public Piece {
+public:
+    Knight(const std::string& color, int row, int column);
+};
 
+class Rook : public Piece {
+public:
+    Rook(const std::string& color, int row, int column);
+};
+
+class Bishop : public Piece {
+public:
+    Bishop(const std::string& color, int row, int column);
+};
+
+class Queen : public Piece {
+public:
+    Queen(const std::string& color, int row, int column);
+};
+
+class King : public Piece {
+public:
+    King(const std::string& color, int row, int column);
 };
